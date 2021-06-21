ff2580a07dbaadc80a33fd1f356481ba04144348
ask <tjdrud3632@naver.com>
2021-06-21 16:19:06 +0900

    6/21 test5

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,15 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=springboot-webservice
 
+echo $PROJECT_NAME
+
 echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.war $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(sudo pgrep -fl $PROJECT_NAME | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
