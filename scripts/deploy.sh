34c3b2e55a1843a3d2e3b10f3e4303b978d3de04
ask <tjdrud3632@naver.com>
2021-06-21 16:53:38 +0900

    6/21 test9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ cp $REPOSITORY/zip/*.war $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl springboot-webservice | awk '{print $1}')
+CURRENT_PID=$(pgrep -f $PROJECT_NAME*.war)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
